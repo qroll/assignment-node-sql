063fd455d0a0ea3c7d0554da25dc56d5a9a140b7
qroll <quek.ruoling@gmail.com>
2018-03-13 12:00:34 +0800

    Initialise db with data

--- db/sql/init.sql
+++ db/sql/init.sql
@@ -1,38 +1,71 @@
+DROP SCHEMA IF EXISTS app;
+CREATE SCHEMA app;
 USE app;
 
 CREATE TABLE teachers (
-  id VARCHAR(36) PRIMARY KEY,
-  email VARCHAR(255) UNIQUE NOT NULL
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE students (
-  id VARCHAR(36) PRIMARY KEY,
-  email VARCHAR(255) UNIQUE NOT NULL,
-  isSuspended BOOLEAN DEFAULT TRUE
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    isSuspended BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE registration (
-  teacherId VARCHAR(36) REFERENCES teachers(id),
-  studentId VARCHAR(36) REFERENCES students(id),
-  PRIMARY KEY (teacherId, studentId)
+    teacherId INT REFERENCES teachers (id),
+    studentId INT REFERENCES students (id),
+    PRIMARY KEY (teacherId , studentId)
 );
 
-DELIMITER $$
-CREATE TRIGGER insert_id_teachers
-BEFORE INSERT ON teachers
-FOR EACH ROW
-BEGIN
-  SET NEW.id = UUID();
-END
-$$
-DELIMITER ;
+-- initialize db with data
 
-DELIMITER $$
-CREATE TRIGGER insert_id_students
-BEFORE INSERT ON students
-FOR EACH ROW
-BEGIN
-  SET NEW.id = UUID();
-END
-$$
-DELIMITER ;
+INSERT INTO teachers (email) VALUES 
+('penelope.fitts@example.com'), 
+('jessica.whitwell@example.com'),
+('j.stroud@example.com');
+
+INSERT INTO students (email) VALUES 
+('lucy.carlyle@gmail.com'), 
+('anthony.lockwood@gmail.com'), 
+('george.cubbins@gmail.com'), 
+('john.mandrake@gmail.com'), 
+('jane.farrar@gmail.com'), 
+('rebecca.piper@gmail.com');
+
+INSERT INTO registration (teacherId, studentId)
+(SELECT t.id, s.id
+FROM teachers t, students s 
+WHERE t.email LIKE 'penelope.fitts@example.com' 
+AND s.email LIKE 'lucy.carlyle@gmail.com');
+
+INSERT INTO registration (teacherId, studentId)
+(SELECT t.id, s.id
+FROM teachers t, students s 
+WHERE t.email LIKE 'penelope.fitts@example.com' 
+AND s.email LIKE 'george.cubbins@gmail.com');
+
+INSERT INTO registration (teacherId, studentId)
+(SELECT t.id, s.id
+FROM teachers t, students s 
+WHERE t.email LIKE 'jessica.whitwell@example.com' 
+AND s.email LIKE 'john.mandrake@gmail.com');
+
+INSERT INTO registration (teacherId, studentId)
+(SELECT t.id, s.id
+FROM teachers t, students s 
+WHERE t.email LIKE 'jessica.whitwell@example.com' 
+AND s.email LIKE 'jane.farrar@gmail.com');
+
+INSERT INTO registration (teacherId, studentId)
+(SELECT t.id, s.id
+FROM teachers t, students s 
+WHERE t.email LIKE 'j.stroud@example.com' 
+AND s.email LIKE 'lucy.carlyle@gmail.com');
+
+INSERT INTO registration (teacherId, studentId)
+(SELECT t.id, s.id
+FROM teachers t, students s 
+WHERE t.email LIKE 'j.stroud@example.com' 
+AND s.email LIKE 'john.mandrake@gmail.com');
