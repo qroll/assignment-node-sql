64ba7fd1978f9badb090958d944882ab1a02dc4e
qroll <quek.ruoling@gmail.com>
2018-03-17 07:34:56 +0800

    Fix db connection

--- server/src/knex.js
+++ server/src/knex.js
@@ -8,23 +8,49 @@ const mockConnection = () => {
     return connection;
 };
 
-const realConnection = () => {
+const development = () => {
     return require("knex")({
         client: "mysql",
         connection: {
-            host:
-                process.env.NODE_ENV === "production"
-                    ? process.env.DB_HOST
-                    : "localhost",
+            host: "localhost",
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
-            database:
-                process.env.NODE_ENV === "test" ? "test" : process.env.DB_NAME
+            database: process.env.DB_NAME
         }
     });
 };
 
-const knex =
-    process.env.NODE_ENV === "unit" ? mockConnection() : realConnection();
+const production = () => {
+    const connectionString =
+        "mysql://" +
+        process.env.DB_USER +
+        ":" +
+        process.env.DB_PASSWORD +
+        "@" +
+        process.env.DB_HOST +
+        ":" +
+        process.env.DB_PORT +
+        "/" +
+        process.env.DB_NAME;
 
-module.exports = knex;
+    return require("knex")({
+        client: "mysql",
+        connection: connectionString
+    });
+};
+
+const knex = () => {
+    switch (process.env.NODE_ENV) {
+        case "unit":
+            return mockConnection();
+            break;
+        case "production":
+            return production();
+            break;
+        default:
+            return development();
+            break;
+    }
+};
+
+module.exports = knex();
